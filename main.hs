71a4cfa31725491b9fe8816c2c145048ae0d5af9
Juan Carlos Zenteno <nanoxmaligno@gmail.com>
2022-12-15 15:26:20 -0600

    Esqueleto de la práctica

---
+++ main.hs
@@ -0,0 +1,22 @@
+-- Actividad 1
+data Edo = Q0 | Q1 | Q2 | Q3 | QF deriving Show
+type Q = [Edo]
+
+-- Actividad 2
+data Simbolo = A | B | X | Y | Blanco deriving Show
+
+-- Actividad 3
+
+-- Actividad 4
+
+-- Actividad 5
+
+-- Actividad 6
+
+-- Actividad 7
+
+-- Extra 1
+
+-- Extra 2
+
+-- Extra 3
